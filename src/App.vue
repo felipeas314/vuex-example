ede3f5a4850ec92a5159369ba93c37f546ea3bbc
Felipe <felipeas314@gmail.com>
2020-07-05 15:57:18 -0300

    login

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
+</template><script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
-</script>
-
-<style>
+</script><style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
+import { store } from './store';
+import { router } from './router';
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
